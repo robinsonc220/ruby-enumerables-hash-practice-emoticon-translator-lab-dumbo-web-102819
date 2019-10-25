03e5dd59367f33674dc1037b22f22407862e179f
Conrad Robinson <robinsonc220@gmail.com>
2019-10-25 17:43:06 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -20,7 +20,7 @@ def get_japanese_emoticon(path, emoticon)
   emoticon_hash = load_library(path)
   result = emoticon_hash["get_emoticon"][emoticon]
   if result == nil
-    result = "Sorry, that emoticon was not found" 
+    result = "Sorry, that emoticon was not found"
   end
   result
 end
@@ -29,7 +29,7 @@ def get_english_meaning(path, emoticon)
   emoticon_hash = load_library(path)
   result = emoticon_hash["get_meaning"][emoticon]
   if result == nil
-    result = "Sorry, that emoticon was not found" 
+    result = "Sorry, that emoticon was not found"
   end
   result
 end
